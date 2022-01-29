752333a551026e5b77525f7f336e9beaceb7b7ae
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-29 10:54:42 -0800

    2 files modified

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
+  public expanded: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  expandCollapse() {
+    this.expanded != this.expanded;
   }
-
 }
