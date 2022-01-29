18be7f07b4d175c7f2c5afbc9e451516fe777155
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-29 10:41:00 -0800

    4 files added

---
+++ src/app/layout-instance/layout-instance.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-instance',
+  templateUrl: './layout-instance.component.html',
+  styleUrls: ['./layout-instance.component.css'],
+})
+export class LayoutInstanceComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
