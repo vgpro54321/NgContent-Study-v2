1f772c78b7e4dd63893ba88621d1416756b82402
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-29 10:43:31 -0800

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutInstanceComponent } from './layout-instance/layout-instance.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    LayoutComponent,
+    LayoutInstanceComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
